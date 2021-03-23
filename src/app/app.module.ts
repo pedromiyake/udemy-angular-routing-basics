3cfaf5de7c5ac9f514f304a7642ac5dba7677d3b
Pedro Miyake <pedro.miyake@top-itservices.com>
2021-03-23 11:41:49 +0100

    Added canDeactivate functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ServersService } from "./servers/servers.service";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { AuthGuardService } from "./auth-guard.service";
 import { AuthService } from "./auth.service";
+import { CanDeactivateGuard } from "./servers/edit-server/can-deactivate-guard.service";
 
 @NgModule({
   declarations: [
@@ -27,7 +28,12 @@ import { AuthService } from "./auth.service";
     PageNotFoundComponent,
   ],
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  providers: [ServersService, AuthService, AuthGuardService],
+  providers: [
+    ServersService,
+    AuthService,
+    AuthGuardService,
+    CanDeactivateGuard,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
